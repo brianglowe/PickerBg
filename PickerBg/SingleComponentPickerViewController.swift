af39891627be9b90e73a69b30a0a7095ed7b821b
Brian Glowe <brianglowe@gmail.com>
2015-10-06 08:48:03 -0400

    Fixed error with SinglePicker Class

--- PickerBg/SingleComponentPickerViewController.swift
+++ PickerBg/SingleComponentPickerViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class SingleComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-
+    
     @IBOutlet weak var singlePicker: UIPickerView!
+    
+    private let characterNames = [
+        "Luke", "Leia", "Han", "Chewbacca", "Artoo", "Threepio", "Lando" ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +39,51 @@ class SingleComponentPickerViewController: UIViewController, UIPickerViewDelegat
     */
 
     @IBAction func buttonPressed(sender: AnyObject) {
+        let row = singlePicker.selectedRowInComponent(0)
+        let selected = characterNames[row]
+        let title = "You selected \(selected)!"
+        
+        let alert = UIAlertController(title: title, message: "Thank you for choosing", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "You're welcome", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
     }
 
-
+    // MARK:-
+    // MARK: Picker Data Source Methods
+   
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return characterNames.count
+    }
+    
+    // MARK: Picker Delegate Methods
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return characterNames[row]
+        }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
